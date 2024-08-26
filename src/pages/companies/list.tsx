41a39393971e66c016252b6ad23108927b7996ca
AGBRO PC <you@example.com>
2024-08-26 23:00:05 +0100

    initial commit

---
+++ src/pages/companies/list.tsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
+import { getDefaultFilter, type HttpError, useGo } from "@refinedev/core";
+import type { GetFieldsFromList } from "@refinedev/nestjs-query";
+
+import { SearchOutlined } from "@ant-design/icons";
+import { Input, Space, Table } from "antd";
+import {Text} from 'components/text' 
+import type { CompaniesListQuery } from "@/graphql/types";
+import { currencyNumber } from "@/utilities";
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
+import { CustomAvatar } from "@/components/custom-avatar";
+
+
+
+type Company = GetFieldsFromList<CompaniesListQuery>;
+
+export const CompanyList = ({ children }: React.PropsWithChildren) => {
+  const go = useGo();
+
+  const { tableProps, filters } = useTable<Company, HttpError, Company>({
+    resource: "companies",
+    onSearch: (values) => {
+      return [
+        {
+          field: "name",
+          operator: "contains",
+          value: values.name,
+        },
+      ];
+    },
+    sorters: {
+      initial: [
+        {
+          field: "createdAt",
+          order: "desc",
+        },
+      ],
+    },
+    filters: {
+      initial: [
+        {
+          field: "name",
+          operator: "contains",
+          value: undefined,
+        },
+      ],
+    },
+    pagination: {
+      pageSize: 12,
+    },
+    meta: {
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
+
+  return (
+    <div className="page-container">
+      <List
+        breadcrumb={false}
+        headerButtons={() => {
+          return (
+            <CreateButton
+              onClick={() => {
+                go({
+                  to: {
+                    resource: "companies",
+                    action: "create",
+                  },
+                  options: {
+                    keepQuery: true,
+                  },
+                  type: "replace",
+                });
+              }}
+            />
+          );
+        }}
+      >
+        <Table
+          {...tableProps}
+          pagination={{
+            ...tableProps.pagination,
+            pageSizeOptions: ["12", "24", "48", "96"],
+            
+          }}
+          rowKey="id"
+        >
+          <Table.Column<Company>
+            dataIndex="name"
+            title="Company title"
+            defaultFilteredValue={getDefaultFilter("id", filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(_, record) => {
+              return (
+                <Space>
+                  <CustomAvatar
+                    shape="square"
+                    name={record.name}
+                    src={record.avatarUrl}
+                  /><Text style={{whiteSpace: "nowrap",
+                    }}
+                  >
+                    {record.name}
+                  </Text>
+                </Space>
+              );
+            }}
+          />
+          <Table.Column<Company>
+            dataIndex={"totalRevenue"}
+            title="Open deals amount"
+            render={(_, company) => {
+              return (
+                <Text>
+                  {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+                </Text>
+              );
+            }}
+          />
+          <Table.Column<Company>
+            fixed="right"
+            dataIndex="id"
+            title="Actions"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+      {children}
+    </div>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export * from './Home';
+export * from './forgotPassword';
+export * from './login';
+export * from './register';
+export * from './categories';
+export * from './blog-posts';
+export * from './companies/list';
+export * from './companies/create';
+export * from './companies/edit';
+
+
+
